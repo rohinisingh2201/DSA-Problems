f0879663f54c5665538fd091f2ef880457e306ed
Rohini Singh <rohinisingh2201@gmail.com>
2023-10-02 18:03:28 +0530

    Create subtree.java

---
+++ subtree.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if(s == null) {
+           return t == null;
+        }
+
+        return isSame(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    public boolean isSame(TreeNode t1, TreeNode t2) {
+        if(t1 == null && t2 == null) {
+            return true;
+        }
+
+        if(t1 == null || t2 == null) {
+            return false;
+        }
+
+        if(t1.val != t2.val) return false;
+
+        return isSame(t1.left, t2.left) && isSame(t1.right, t2.right);
+    }
+}
